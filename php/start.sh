8d2a7e237d83430856bde8df513c3d8f96048b99
wbxl0 <917661154@qq.com>
2026-02-22 23:25:05 +0800

    Update start.sh

--- php/start.sh
+++ php/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 
 export UUID=${UUID:-'3913084d-9c58-4b49-a259-b77e471133f5'} # 如开启哪吒v1,不同的平台需要改一下，否则会覆盖
-export NEZHA_SERVER=${NEZHA_SERVER:-'v1.wbxl.dpdns.org:10024'}       # v1哪吒填写形式：nezha.xxx.com:8008,v0哪吒填写形式：nezha.xxx.com
+export NEZHA_SERVER=${NEZHA_SERVER:-'nz.wbxl.dpdns.org:443'}       # v1哪吒填写形式：nezha.xxx.com:8008,v0哪吒填写形式：nezha.xxx.com
 export NEZHA_PORT=${NEZHA_PORT:-''}           # v1哪吒不要填写这个,v0哪吒agent端口为{443,8443,2053,2083,2087,2096}其中之一时自动开启tls
-export NEZHA_KEY=${NEZHA_KEY:-'fXYWSXfEZbSHMuGHEhcJjisCWDInLBtR'}             # 哪吒v0-agent密钥或v1的NZ_CLIENT_SECRET
+export NEZHA_KEY=${NEZHA_KEY:-'eQznXSiec5C101xYWVMZQiTrpVUnEAFc'}             # 哪吒v0-agent密钥或v1的NZ_CLIENT_SECRET
 export ARGO_DOMAIN=${ARGO_DOMAIN:-''}         # 固定隧道域名,留空即启用临时隧道
 export ARGO_AUTH=${ARGO_AUTH:-''}             # 固定隧道token或json,留空即启用临时隧道,json获取:https://json.zone.id
 export CFIP=${CFIP:-'saas.sin.fan'}           # argo节点优选域名或优选ip
